30a87211e1f91b89e43300098a787315d15732dd
Fuat Akca <37161227+muratKeys@users.noreply.github.com>
2019-05-20 18:50:09 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include "gpio.h"
 
-#define TASK_STKSZ	60	//default stack size
+#define TASK_STKSZ	60	//default stack size. //Dont forget set it to 0 for defauld
 #define TASK_PRIO	20	//0-99
 #define TASK_MODE	0	//no flags
 
